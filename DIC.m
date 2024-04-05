e069679713871742ee5bf553065017311d781c9d
Aidan <aidan@mcdcentral.org>
2024-04-05 17:23:30 -0600

    Add hole sample

--- DIC.m
+++ DIC.m
@@ -2,46 +2,74 @@ clear;
 close all;
 clc;
 
-load('rotation_data.mat')
+
+%% Load input files
+filedirectory = 'Hole_Plate_Images';
+filename = 'ohtcfrp_';
+ref = read(Tiff(strcat(filedirectory,'/',filename,'01','.tif')));
+ref = double(ref)/256.;
+cur = read(Tiff(strcat(filedirectory,'/',filename,'11','.tif')));
+cur = double(cur)/256.;
+
+
+%load('rotation_data.mat')
 
 % cur = imrotate(cur, 3);
 % cur = cur(round(size(cur)/2)-188:round(size(cur)/2)+187,round(size(cur)/2)-188:round(size(cur)/2)+187);
 % 
 % cur = imresize(cur,1.15);
 % cur = cur(round(size(cur)/2)-188:round(size(cur)/2)+187,round(size(cur)/2)-188:round(size(cur)/2)+187);
-
-Fxx = 1.01;
-Fxy = -0.05;
-Fyx = -.001;
-Fyy = .99;
-tform = affine2d([Fxx Fxy 0; Fyx Fyy 0; 0 0 1]);
-cur = imwarp(ref,tform);
+% 
+% Fxx = 1.01;
+% Fxy = -0.05;
+% Fyx = -.001;
+% Fyy = .99;
+% tform = affine2d([Fxx Fxy 0; Fyx Fyy 0; 0 0 1]);
+% cur = imwarp(ref,tform);
 % cur = cur(round(size(cur)/2)-188:round(size(cur)/2)+187,round(size(cur)/2)-188:round(size(cur)/2)+187);
 
 %% Create unstructured grid
+backgroundIntensityCutoff = .1;
+
+% candidateGridSize = 1000;
+% buffer = 30;
+% 
+% candidateGridX = round((size(ref,2)-2*buffer)*rand(candidateGridSize,1)+buffer);
+% candidateGridY = round((size(ref,1)-2*buffer)*rand(candidateGridSize,1)+buffer);
+% grid = [];
+% for i=1:candidateGridSize
+%     if ref(candidateGridY(i),candidateGridX(i))>backgroundIntensityCutoff
+%         grid = [grid; [candidateGridX(i) candidateGridY(i)]];
+%     end
+% end
+
+
 candidateGridDimensionX = 30;
-candidateGridDimensionY = 30;
-candidateGridX = round(linspace(1, size(ref,1), candidateGridDimensionX));
-candidateGridY = round(linspace(1, size(ref,2), candidateGridDimensionY));
+candidateGridDimensionY = 50;
+%candidateGridX = round(linspace(1, size(ref,2), candidateGridDimensionX));
+%candidateGridY = round(linspace(1, size(ref,1), candidateGridDimensionY));
+candidateGridX = round(linspace(75, size(ref,2)-75, candidateGridDimensionX));
+candidateGridY = round(linspace(75, size(ref,1)-75, candidateGridDimensionY));
+% candidateGridY = round(linspace(400, 640, candidateGridDimensionX));
+% candidateGridX = round(linspace(80, 320, candidateGridDimensionY));
 
 grid = [];
-for i=1:length(candidateGridX)
-    for j=1:length(candidateGridY)
-        if ref(candidateGridX(i),candidateGridY(j))>1e-2
-            grid = [grid; [candidateGridX(i) candidateGridY(j)]];
+for i=1:size(candidateGridY,2)
+    for j=1:size(candidateGridX,2)
+        if ref(candidateGridY(i),candidateGridX(j))>backgroundIntensityCutoff
+            grid = [grid; [candidateGridX(j) candidateGridY(i)]];
         end
     end
 end
 
 
 %% Get displacements with normxcorr2
-
 displacementsList = [];
 for i=1:length(grid)
     subImageX = grid(i,1);
     subImageY = grid(i,2);
-    width = 10;
-    height = 10;
+    width = 20;
+    height = 20;
     curSubimageBuffer = 20;
 
     refSubImageTopLeftY = round(subImageY-height/2);
@@ -82,6 +110,19 @@ gridY = grid(:,2);
 %DT = delaunayTriangulation(grid(:,1),grid(:,2));
 DT = delaunay(grid(:,1),grid(:,2));
 
+%Get rid of triangles whose centroids are background
+trianglesToRemove = [];
+for i = 1:size(DT,1)
+    triangle = DT(i,:);
+    centroid = round([(grid(triangle(1),1)+grid(triangle(2),1)+grid(triangle(3),1))/3,(grid(triangle(1),2)+grid(triangle(2),2)+grid(triangle(3),2))/3]);
+    if ref(centroid(2),centroid(1))<backgroundIntensityCutoff
+        trianglesToRemove = [trianglesToRemove; i];
+    end
+end
+for i = 1:size(trianglesToRemove)
+    DT(trianglesToRemove(end+1-i),:)=[];
+end
+
 % k = 1;
 % for i=1:length(grid)
 %    displacementsMatrix(j,i,:) = [displacementsList(k,3:4)];
@@ -109,12 +150,12 @@ title("Subregions in deformed image")
 %% Scatterplot of displacements
 % Multiply y by -1 since image starts in the upper left
 figure();
-scatter(displacementsList(:,1),-displacementsList(:,2)+size(ref,2), 40, 'red')
+scatter(displacementsList(:,1),-displacementsList(:,2)+size(ref,1), 40, 'red')
 hold on;
-scatter(displacementsList(:,1)+displacementsList(:,3), -displacementsList(:,2)-displacementsList(:,4)+size(ref,2), 40, 'green')
+scatter(displacementsList(:,1)+displacementsList(:,3), -displacementsList(:,2)-displacementsList(:,4)+size(ref,1), 40, 'green')
 hold off;
-xlim([0,size(ref,1)])
-ylim([0,size(ref,2)])
+xlim([0,size(ref,2)])
+ylim([0,size(ref,1)])
 title("Grid displacement")
 xlabel("x (pixels)")
 ylabel("y (pixels)")
@@ -123,20 +164,20 @@ ylabel("y (pixels)")
 figure();
 tiledlayout(1,2);
 nexttile
-[~,h] = tricontf(gridX, -gridY+size(ref,2), DT, displacementsList(:,3));
+[~,h] = tricontf(gridX, -gridY+size(ref,1), DT, displacementsList(:,3));
 set(h,'edgecolor','none');
 colorbar
-xlim([0,size(ref,1)])
-ylim([0,size(ref,2)])
+xlim([0,size(ref,2)])
+ylim([0,size(ref,1)])
 title("x displacement")
 xlabel("x (pixels)")
 ylabel("y (pixels)")
 nexttile
-[~,h] = tricontf(gridX, -gridY+size(ref,2), DT, -displacementsList(:,2));
+[~,h] = tricontf(gridX, -gridY+size(ref,1), DT, -displacementsList(:,4));
 set(h,'edgecolor','none');
 colorbar
-xlim([0,size(ref,1)])
-ylim([0,size(ref,2)])
+xlim([0,size(ref,2)])
+ylim([0,size(ref,1)])
 title("y displacement")
 xlabel("x (pixels)")
 ylabel("y (pixels)")
@@ -173,30 +214,30 @@ end
 figure()
 tiledlayout(2,2);
 nexttile
-[~,h] = tricontf(gridX, -gridY+size(ref,2), DT, epsilon(:,1,1));
+[~,h] = tricontf(gridX, -gridY+size(ref,1), DT, epsilon(:,1,1));
 set(h,'edgecolor','none');
 colorbar
 title("\epsilonxx")
-xlim([0,size(ref,1)])
-ylim([0,size(ref,2)])
+xlim([0,size(ref,2)])
+ylim([0,size(ref,1)])
 nexttile
-[~,h] = tricontf(gridX, -gridY+size(ref,2), DT, epsilon(:,2,2));
+[~,h] = tricontf(gridX, -gridY+size(ref,1), DT, epsilon(:,2,2));
 set(h,'edgecolor','none');
 colorbar
 title("\epsilonyy")
-xlim([0,size(ref,1)])
-ylim([0,size(ref,2)])
+xlim([0,size(ref,2)])
+ylim([0,size(ref,1)])
 nexttile
-[~,h] = tricontf(gridX, -gridY+size(ref,2), DT, epsilon(:,1,2));
+[~,h] = tricontf(gridX, -gridY+size(ref,1), DT, epsilon(:,1,2));
 set(h,'edgecolor','none');
 colorbar
 title("\epsilonxy")
-xlim([0,size(ref,1)])
-ylim([0,size(ref,2)])
+xlim([0,size(ref,2)])
+ylim([0,size(ref,1)])
 nexttile
-[~,h] = tricontf(gridX, -gridY+size(ref,2), DT, omega(:,1,2));
+[~,h] = tricontf(gridX, -gridY+size(ref,1), DT, omega(:,1,2));
 set(h,'edgecolor','none');
 colorbar
 title("\omegaxy")
-xlim([0,size(ref,1)])
-ylim([0,size(ref,2)])
+xlim([0,size(ref,2)])
+ylim([0,size(ref,1)])
